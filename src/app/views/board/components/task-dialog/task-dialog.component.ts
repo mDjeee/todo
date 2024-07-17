60e83102203cd88220991f33ea6bb6fabee6176f
mdjeee <i.sejtniyazov@mail.ru>
2024-07-17 18:10:14 +0500

    feat: add dialog component

---
+++ src/app/views/board/components/task-dialog/task-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-task-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-dialog.component.html',
+  styleUrl: './task-dialog.component.scss'
+})
+export class TaskDialogComponent {
+}
--- src/app/views/board/components/task/task.component.ts
+++ src/app/views/board/components/task/task.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ITask } from '../../interfaces/task.interface';
 import { MatIcon } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent } from '../task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-task',
@@ -13,4 +15,11 @@ import { MatIcon } from '@angular/material/icon';
 })
 export class TaskComponent {
   @Input() task!: ITask;
+  dialog = inject(MatDialog);
+
+  openDialog() {
+    const doalogRef = this.dialog.open(TaskDialogComponent, {
+      data: this.task
+    });
+  }
 }
